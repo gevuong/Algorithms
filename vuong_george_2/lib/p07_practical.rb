89e7429a6f7ec50741848ba35b077a933cbcf50e
George V <georgevuong18@gmail.com>
2017-08-16 17:32:41 -0700

    init commit algos project 3

--- vuong_george_2/lib/p07_practical.rb
+++ vuong_george_2/lib/p07_practical.rb
@@ -3,3 +3,7 @@ require_relative 'p05_hash_map'
 def can_string_be_palindrome?(string)
 
 end
+
+# create a hashmap to count up the number of letters
+# if word is an even number lenth, then all letters counts need to be an even number
+# if word is odd number length, then all letters counts need to be an even number except one of the entries
