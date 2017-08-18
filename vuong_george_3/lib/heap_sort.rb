d6c9827114b848aabae505a6eacc061cc08a5183
George V <georgevuong18@gmail.com>
2017-08-18 10:18:40 -0700

    init commit

--- vuong_george_3/lib/heap_sort.rb
+++ vuong_george_3/lib/heap_sort.rb
@@ -3,10 +3,11 @@ require_relative "heap"
 class Array
   def heap_sort!
     (2..length).each do |size|
+      # want to reduce heap size per iteration
       BinaryMinHeap.heapify_up(self, size - 1, size)
     end
 
-    length.downto(2).each do |size|
+    (2..length).to_a.reverse.each do |size|
       self[size - 1], self[0] = self[0], self[size - 1]
       BinaryMinHeap.heapify_down(self, 0, size - 1)
     end
--- vuong_george_3/lib/k_largest_elements.rb
+++ vuong_george_3/lib/k_largest_elements.rb
@@ -5,4 +5,12 @@ def k_largest_elements(array, k)
   sorted_arr = array.heap_sort!
   arr = sorted_arr.drop(array.length - k)
   arr
+
+  res = BinaryMinHeap.new
+  k.times do
+    result.push(array.pop) # k * log(k)
+  end
+
+  until array.empty?
+    n - klogk
 end
