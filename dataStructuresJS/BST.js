7e3285001e26b6f8917ba9305a153f248efa567b
George V <georgevuong18@gmail.com>
2017-10-08 00:04:24 -0700

    add test case for insert method

--- dataStructuresJS/BST.js
+++ dataStructuresJS/BST.js
@@ -19,7 +19,19 @@ BST.prototype.insert = function(value) {
 }
 
 // Test above case:
-var BST = new BST(20);
-BST.insert(29);
-BST.insert(13);
-console.log(BST);
+var bst = new BST(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(100);
+bst.insert(60);
+bst.insert(59);
+bst.insert(20);
+bst.insert(45);
+bst.insert(35);
+bst.insert(85);
+bst.insert(105);
+bst.insert(10);
+console.log(bst); // not very helpful
+console.log(bst.right.left.left); // => 59
+console.log(bst.left.right.left); // => 35
+console.log(bst.right.right); // => 100
