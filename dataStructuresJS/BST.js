ab2eeb7de2196b856aa1700223189f815e859f8d
George V <georgevuong18@gmail.com>
2018-04-23 00:27:08 -0700

    update BST.js

--- dataStructuresJS/BST.js
+++ dataStructuresJS/BST.js
@@ -18,7 +18,7 @@ BST.prototype.insert = function(value) {
     if (!this.right) this.right = new BST(value);
     else this.right.insert(value);
   }
-}
+};
 // test above code:
 var bst = new BST(6);
 bst.insert(4);
@@ -30,14 +30,14 @@ bst.insert(8);
 bst.insert(7);
 bst.insert(9);
 console.log('----- test #insert method -----');
-console.log(bst.left.left.right.value == 3);
-console.log(bst.left.left.left.value == 1);
-console.log(bst.right.left.value == 7);
+console.log(bst.left.left.right.value === 3);
+console.log(bst.left.left.left.value === 1);
+console.log(bst.right.left.value === 7);
 
 
 // #contains method (use recursion)
 BST.prototype.contains = function(value) {
-  if (value == this.value) return true;
+  if (value === this.value) return true;
   if (value < this.value) {
     if (!this.left) return false;
     else return this.left.contains(value);
@@ -46,22 +46,22 @@ BST.prototype.contains = function(value) {
     if (!this.right) return false;
     else return this.right.contains(value);
   }
-}
+};
 // test above code:
 console.log('----- test #contains method -----');
-console.log(bst.contains(4) == true);
-console.log(bst.contains(9) == true);
-console.log(bst.contains(0) == false);
+console.log(bst.contains(4) === true);
+console.log(bst.contains(9) === true);
+console.log(bst.contains(0) === false);
 
 
 // #depthFirstSearch method
 BST.prototype.depthFirstSearch = function(logValueFunc, order) {
-  if (order == 'pre-order') logValueFunc(this.value)
-  if (this.left)  this.left.depthFirstSearch(logValueFunc, order)
-  if (order == 'in-order') logValueFunc(this.value)
-  if (this.right) this.right.depthFirstSearch(logValueFunc, order)
-  if (order == 'post-order') logValueFunc(this.value)
-}
+  if (order === 'pre-order') logValueFunc(this.value);
+  if (this.left)  this.left.depthFirstSearch(logValueFunc, order);
+  if (order === 'in-order') logValueFunc(this.value);
+  if (this.right) this.right.depthFirstSearch(logValueFunc, order);
+  if (order === 'post-order') logValueFunc(this.value);
+};
 
 function logValueFunc(value) {
   console.log(value);
@@ -69,7 +69,7 @@ function logValueFunc(value) {
 // test above code:
 console.log('------ test #depthFirstSearch ------');
 console.log('in-order');
-bst.depthFirstSearch(logValueFunc, 'in-order'); // values should logged from least to greatest
+bst.depthFirstSearch(logValueFunc, 'in-order'); // values should log from least to greatest
 console.log('pre-order');
 bst.depthFirstSearch(logValueFunc, 'pre-order');
 console.log('post-order');
@@ -78,14 +78,14 @@ bst.depthFirstSearch(logValueFunc, 'post-order');
 
 // #breadthFirstSearch method (no recursion)
 BST.prototype.breadthFirstSearch = function(logValueFunc) {
-  queue = [this]; // initially stores root node
+  let queue = [this]; // initially stores root node
   while (queue.length) {
     var treeNode = queue.shift(); // store first node from queue in treeNode
     logValueFunc(treeNode.value);
     if (treeNode.left) queue.push(treeNode.left);
     if (treeNode.right) queue.push(treeNode.right);
   }
-}
+};
 // test above code:
 console.log('----- test #breadthFirstSearch ------');
 bst.breadthFirstSearch(logValueFunc);
@@ -95,13 +95,13 @@ bst.breadthFirstSearch(logValueFunc);
 BST.prototype.getMinVal = function() {
   if (!this.left) return this.value;
   else return this.left.getMinVal();
-}
+};
 // #getMaxVal method
 BST.prototype.getMaxVal = function() {
   if(!this.right) return this.value;
   else return this.right.getMaxVal();
-}
+};
 // test above code:
 console.log('----- test getMinVal, getMaxVal -------');
-console.log('getMinVal', bst.getMinVal() == 1);
-console.log('getMaxVal', bst.getMaxVal() == 9);
+console.log('getMinVal', bst.getMinVal() === 1);
+console.log('getMaxVal', bst.getMaxVal() === 9);
